4d5ceb6d2a68804880131817805fe4ee14a25ada
AymericGaudin <aymeric.gaudin@cea.fr>
2026-08-26 09:22:07 +0200

    caravel/loaders/_mp4: method checking actual file type for mp4 (instead of trusting extension)

--- caravel/loaders/_mp4.py
+++ caravel/loaders/_mp4.py
@@ -10,11 +10,33 @@
 This module defines the mp4 dataset loader.
 """
 
+# Imports
+import subprocess
+
 # Third party import
 import imageio
 
 # Package import
 from .loader_base import LoaderBase
+
+
+def is_mp4(path, verbose=False):
+    """Check if a file is truly a mp4 file by checking its actual content.
+    It uses the 'file' command line to do so.
+
+    Parameters
+    ----------
+    path: str
+        Path to the file to be checked.
+    verbose: bool, default False
+        Whether to print or not the 'file' command output.
+    """
+    cmd = ["file", path]
+    output = subprocess.check_output(cmd).decode()
+    if verbose: print(output)
+    output = output.strip(path)
+    output = output.lower()
+    return "mp4" in output
 
 
 class MP4(LoaderBase):
@@ -35,6 +57,7 @@ class MP4(LoaderBase):
         data: imageio numpy array
             the loaded image.
         """
+        assert is_mp4(path, verbose=False), f"{path} is not a mp4 file"
         return imageio.get_reader(path,  'ffmpeg')
 
     def save(self, data, outpath, fps=24):
